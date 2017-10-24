04ea0409308609a14835b1dccadf6ff4c5e23d2a
Rahul Kumar <29652757+rahuls321@users.noreply.github.com>
2017-10-24 13:27:02 +0530

    Add files via upload

---
+++ Supervised Learning with scikit-learn/Regression/Cross validation/5-fold cross-validation.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 24 12:42:22 2017
+
+@author: hp
+"""
+
+""" 5-fold cross-validation """
+
+
+
+# Import the necessary modules
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import cross_val_score
+
+# Create a linear regression object: reg
+reg = LinearRegression()
+
+# Compute 5-fold cross-validation scores: cv_scores
+cv_scores = cross_val_score(reg,X,y, cv = 5)
+
+# Print the 5-fold cross-validation scores
+print(cv_scores)
+
+print("Average 5-Fold CV Score: {}".format(np.mean(cv_scores)))
